3513988722dfcefa571956de1d6f921ce909eeb3
madinsane <abrownlow1@gmail.com>
2022-10-06 20:17:46 +0100

    Update boilerplate for use with my domain

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Bismarck">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
